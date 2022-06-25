77831c066123811902bbbe4a4e744eea5c46db5d
csamudra <100887431+csamudra@users.noreply.github.com>
2022-06-25 10:11:08 -0700

    Update create_lef.py

--- utils/create_lef.py
+++ utils/create_lef.py
@@ -1,10 +1,3 @@
-### BELOW is the edited version of the file copied from bsg_fakeram
-## Updates :
-#  Removed "flip" to allow pins only on a single layer
-#  Added pin offset == pin_pitch
-#  Added changes made by James Cherry
-#  Changed obstruction shapes
-
 import os
 import sys
 import math
@@ -28,11 +21,12 @@ def create_lef( mem ):
     addr_width  = math.ceil(math.log2(mem.depth))
 
     # Process parameters
-    min_pin_width   = mem.process.pinWidth_um
-    min_pin_pitch   = mem.process.pinPitch_um
+    min_pin_width   = mem.process.pin_width_um
+    min_pin_pitch   = mem.process.pin_pitch_um
     manufacturing_grid_um = mem.process.manufacturing_grid_um
-    metalPrefix     = mem.process.metalPrefix
-    metalLayer      = mem.process.metalLayer
+    metal_prefix     = mem.process.metal_prefix
+    metal_layer      = mem.process.metal_layer
+    column_mux_factor = mem.process.column_mux_factor
  
     # Offset from bottom edge to first pin
     x_offset = 1 * min_pin_pitch   ;# as told by MSK 
@@ -40,8 +34,8 @@ def create_lef( mem ):
     #########################################
     # Calculate the pin spacing (pitch)
     #########################################
-
-    number_of_pins = 3*bits + addr_width + 3 # 3 * bits -> write data, read data, write mask; + 3 -> clk, we_i, ce_i
+    number_of_pins = 2*bits + addr_width + 3
+    #number_of_pins = 3*bits + addr_width + 3 # 3 * bits -> write data, read data, write mask; + 3 -> clk, we_i, ce_i
     number_of_tracks_available = math.floor((h - 2*y_offset) / min_pin_pitch)
     number_of_spare_tracks = number_of_tracks_available - number_of_pins
 
@@ -57,12 +51,12 @@ def create_lef( mem ):
     track_count -= 1
 
     pin_pitch = min_pin_pitch * track_count
-    #group_pitch = math.floor((number_of_tracks_available - number_of_pins*track_count) / 4)*mem.process.pinPitch_um #what is this / 4 ?
+    #group_pitch = math.floor((number_of_tracks_available - number_of_pins*track_count) / 4)*mem.process.pin_pitch_um #what is this / 4 ?
     ## the following 4 lines are changes made by JC
     extra = math.floor((number_of_tracks_available - number_of_pins*track_count) / 4)
     if extra == 0:
         extra = 1
-    group_pitch = extra*mem.process.pinPitch_um
+    group_pitch = extra*mem.process.pin_pitch_um
     #########################################
     # LEF HEADER
     #########################################
@@ -79,11 +73,11 @@ def create_lef( mem ):
     # LEF SIGNAL PINS
     ########################################
 
-    y_step = y_offset - (y_offset % manufacturing_grid_um) + (mem.process.pinWidth_um/2.0)
-    for i in range(int(bits)) :
-        y_step = lef_add_pin( fid, mem, 'w_mask_in[%d]'%i, True, y_step, pin_pitch ) # lef_add_pin returns y_step + pitch; essentially y_step = y_step + pitch
+    y_step = y_offset - (y_offset % manufacturing_grid_um) + (mem.process.pin_width_um/2.0)
+    #for i in range(int(bits)) :
+    #    y_step = lef_add_pin( fid, mem, 'w_mask_in[%d]'%i, True, y_step, pin_pitch ) # lef_add_pin returns y_step + pitch; essentially y_step = y_step + pitch
 
-    y_step += group_pitch-pin_pitch # why is this required ? I think what is happening that the types of pins are divided into 5 groups : w_mask bus, rd_out bus, wd_in bus, addr_in bus and misc. These "groups" are not separated by the pin_pitch, rather they're separated by more distance ( a function of group_pitch and pin pitch )
+    #y_step += group_pitch-pin_pitch # why is this required ? I think what is happening that the types of pins are divided into 5 groups : w_mask bus, rd_out bus, wd_in bus, addr_in bus and misc. These "groups" are not separated by the pin_pitch, rather they're separated by more distance ( a function of group_pitch and pin pitch )
     for i in range(int(bits)) :
         y_step = lef_add_pin( fid, mem, 'rd_out[%d]'%i, False, y_step, pin_pitch )
 
@@ -107,7 +101,7 @@ def create_lef( mem ):
     supply_pin_width = min_pin_width*4
     supply_pin_half_width = supply_pin_width/2
     supply_pin_pitch = min_pin_pitch*8
-    #supply_pin_layer = '%s' % metalLayer
+    #supply_pin_layer = '%s' % metal_layer
 
     ## Create supply pins  : HOW'RE we ensuring that supply pins don't overlap with the signal pins ? Is it by giving x_offset as the base x coordinate ?
     y_step = y_offset
@@ -115,7 +109,7 @@ def create_lef( mem ):
     fid.write('    DIRECTION INOUT ;\n')
     fid.write('    USE GROUND ;\n')
     fid.write('    PORT\n')
-    fid.write('      LAYER %s ;\n' % metalLayer)
+    fid.write('      LAYER %s ;\n' % metal_layer)
     while y_step <= h - y_offset:
         fid.write('      RECT %.3f %.3f %.3f %.3f ;\n' % (x_offset, y_step-supply_pin_half_width, w-x_offset, y_step+supply_pin_half_width))
         y_step += supply_pin_pitch*2 # this *2 is important because we want alternate VDD and VSS pins
@@ -127,7 +121,7 @@ def create_lef( mem ):
     fid.write('    DIRECTION INOUT ;\n')
     fid.write('    USE POWER ;\n')
     fid.write('    PORT\n')
-    fid.write('      LAYER %s ;\n' % metalLayer)
+    fid.write('      LAYER %s ;\n' % metal_layer)
     while y_step <= h - y_offset:
         fid.write('      RECT %.3f %.3f %.3f %.3f ;\n' % (x_offset, y_step-supply_pin_half_width, w-x_offset, y_step+supply_pin_half_width))
         y_step += supply_pin_pitch*2
@@ -145,11 +139,11 @@ def create_lef( mem ):
     ################
 
     # No pins (full rect)  
-    pin_layer_number = metalLayer.replace(metalPrefix, "", 1)
+    pin_layer_number = metal_layer.replace(metal_prefix, "", 1)
     print("Pin layer number is", pin_layer_number)
     for x in range(int(pin_layer_number) - 1) :
       dummy = x + 1
-      fid.write('    LAYER %s%d ;\n' % (metalPrefix,dummy))
+      fid.write('    LAYER %s%d ;\n' % (metal_prefix,dummy))
       fid.write('    RECT 0 0 %.3f %.3f ;\n' % (w,h))
 
     ################
@@ -157,21 +151,21 @@ def create_lef( mem ):
     ################
 
     # No pins (full rect)
-    #fid.write('    LAYER %s2 ;\n' % metalPrefix)
+    #fid.write('    LAYER %s2 ;\n' % metal_prefix)
     #fid.write('    RECT 0 0 %.3f %.3f ;\n' % (w,h))
 
     ################
     # Layer 3
     ################
 
-    #fid.write('    LAYER %s3 ;\n' % metalPrefix)
+    #fid.write('    LAYER %s3 ;\n' % metal_prefix)
     #fid.write('    RECT 0 0 %.3f %.3f ;\n' % (w,h))
 
     ################
     # Layer 4
     ################
 
-    fid.write('    LAYER %s%d ;\n' % (metalPrefix,int(pin_layer_number)))
+    fid.write('    LAYER %s%d ;\n' % (metal_prefix,int(pin_layer_number)))
 
     # Flipped therefore only vertical pg straps
    
@@ -201,7 +195,7 @@ def create_lef( mem ):
     # Walk through same calculation as pins and draw from bottom of the
     # current pin to the top of last pin (start with bottom edge)
     prev_y = 0
-    y_step = y_offset - (y_offset % manufacturing_grid_um) + (mem.process.pinWidth_um/2.0)
+    y_step = y_offset - (y_offset % manufacturing_grid_um) + (mem.process.pin_width_um/2.0)
 
     for i in range(int(bits)) :
         fid.write('    RECT 0 %.3f %.3f %.3f ;\n' % (prev_y,min_pin_width,y_step-min_pin_width/2))
@@ -247,9 +241,9 @@ def create_lef( mem ):
 # y_step = lef_add_pin( fid, mem, 'w_mask_in[%d]'%i, True, y_step, pin_pitch )
 def lef_add_pin( fid, mem, pin_name, is_input, y, pitch ):
 
-  layer = mem.process.metalLayer
-  pw  = mem.process.pinWidth_um
-  hpw = (mem.process.pinWidth_um/2.0) ;# half pin width
+  layer = mem.process.metal_layer
+  pw  = mem.process.pin_width_um
+  hpw = (mem.process.pin_width_um/2.0) ;# half pin width
 
   fid.write('  PIN %s\n' % pin_name)
   fid.write('    DIRECTION %s ;\n' % ('INPUT' if is_input else 'OUTPUT'))
